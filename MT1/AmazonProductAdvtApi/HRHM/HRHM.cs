fe0cc536b552475695ba506515d9cec04489d8ed
kuttsun <tsukushimail+github@gmail.com>
2017-11-04 00:50:29 +0900

    ちょこっと実装

--- MT1/AmazonProductAdvtApi/HRHM/HRHM.cs
+++ MT1/AmazonProductAdvtApi/HRHM/HRHM.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
 
+using System.Xml;
 using System.Xml.Serialization;
 using System.Text.RegularExpressions;
 
@@ -65,6 +66,50 @@ namespace MT1.AmazonProductAdvtApi.HRHM
         /// <param name="args"></param>
         public async void Run()
         {
+            logger.LogInformation("----- HRHM Begin -----");
+
+            try
+            {
+                // セールの一覧を取得
+                await ItemSearchAsync("569298");
+            }
+            catch (Exception e)
+            {
+                logger.LogError("一覧取得失敗" + e.Message);
+            }
+
+            logger.LogInformation("----- HRHM End -----");
+        }
+
+        /// <summary>
+        /// 指定したページのアイテム情報を取得する
+        /// </summary>
+        /// <param name="saleInformation"></param>
+        /// https://images-na.ssl-images-amazon.com/images/G/09/associates/paapi/dg/index.html?ItemSearch.html
+        async Task<bool> ItemSearchAsync(string nodeId)
+        {
+            IDictionary<string, string> request = new Dictionary<string, String>
+            {
+                ["Service"] = service,
+                ["Version"] = apiVersion,
+                ["Operation"] = "ItemSearch",
+                ["SearchIndex"] = "Music",
+                ["ResponseGroup"] = "Medium",
+                ["BrowseNode"] = nodeId
+            };
+
+            logger.LogInformation($"CD 情報一覧取得開始");
+
+            // リクエストを送信して xml を取得
+            var result = await GetXmlAsync(request);
+
+            // 取得した XML を読み込む
+            XmlDocument doc = new XmlDocument();
+            doc.Load(result);
+
+            WriteXml(doc, options.GetNodeListFilePath());
+
+            return true;
         }
     }
 }
