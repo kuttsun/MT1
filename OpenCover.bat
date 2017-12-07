aacccae47e4c6eed57c56d6b255e2dbca292a243
kuttsun <tsukushimail+github@gmail.com>
2017-12-08 00:25:19 +0900

    カバレッジ計測を追加

---
+++ OpenCover.bat
@@ -0,0 +1,19 @@
+﻿rem ソリューションルートで実行すること
+
+rem OpenCover のインストール先
+set OPENCOVER="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
+
+rem テストコマンド
+set TARGET="C:\Program Files\dotnet\dotnet.exe"
+
+rem テストコマンド引数
+set TARGET_TEST="test MT1.Tests\MT1.Tests.csproj"
+
+rem OpenCover の出力ファイル
+set OUTPUT="coverage.xml"
+
+rem カバレッジ計測対象の指定
+set FILTERS="+[*]*"
+
+rem OpenCover の実行
+%OPENCOVER% -register:user -target:%TARGET% -targetargs:%TARGET_TEST% -filter:%FILTERS% -oldstyle -output:%OUTPUT%
---
+++ ReportGenerator.bat
@@ -0,0 +1,13 @@
+﻿rem ソリューションルートで実行すること
+
+rem ReportGenerator のインストール先
+set REPORTGEN="%USERPROFILE%\.nuget\packages\ReportGenerator\3.0.2\tools\ReportGenerator.exe"
+
+rem OpenCover の出力ファイル
+set OUTPUT="coverage.xml"
+
+rem ReportGenerator の HTML 出力先
+set OUTPUT_DIR="Coverage"
+
+rem レポートの生成 (xml から html へ変換)
+%REPORTGEN% -reports:%OUTPUT% -targetdir:%OUTPUT_DIR%
